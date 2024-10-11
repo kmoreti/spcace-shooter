b128824c06f07a8f0001233aa232e17089f23168
Kleber Moreti de Camargo <kleber.moreti.camargo@gmail.com>
2024-10-11 22:10:46 +0100

    WIP

---
+++ scenes/player.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100,500)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += Vector2(1, 0) * 50 * delta
