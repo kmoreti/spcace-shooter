5212db81a542facee686eee4700e2a970cfacd18
Kleber Moreti de Camargo <kleber.moreti.camargo@gmail.com>
2024-10-11 22:39:54 +0100

    Add assets

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+@export var speed := 500
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = Vector2(100,500)
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Vector2(1, 0) * 50 * delta
-	$PlayerImage.rotation += 0.1 * delta
+	var direction = Input.get_vector("left","right","up","down")
+	position += direction * speed * delta
