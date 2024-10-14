ff6b108e467767094b1391b3b4dfd0c8984c2843
Juliana Oliveira Wey Moreti <julianaoliveiraweymoreti@Julianas-MacBook-Air.local>
2024-10-14 22:34:50 +0100

    add collisions

---
+++ scenes/meteor.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@export var speed: int
+@export var rotation_speed: int
+@export var direction_x: float
+
+func _ready() -> void:
+	var rng := RandomNumberGenerator.new()
+	
+	# texture
+	var path: String = "res://graphics/meteors/" + str(rng.randi_range(1,8)) + ".png"
+	$MeteorImage.texture = load(path)
+	
+	# start position
+	var width := get_viewport().get_visible_rect().size[0]
+	var random_x := rng.randi_range(0, width)
+	var randon_y := rng.randi_range(-150, -50)
+	position = Vector2(random_x, randon_y)
+	
+	# speed / rotation / direction
+	speed = rng.randi_range(200, 500)
+	rotation_speed = rng.randi_range(40, 100)
+	direction_x = randf_range(-1, 1)
+
+func _process(delta: float) -> void:
+	position += Vector2(direction_x, 1.0) * speed * delta
+	rotation_degrees += rotation_speed * delta
+	
+func _on_body_entered(body: Node2D) -> void:
+	print("body entered")
+	print(body.name)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed := 500
 # Called when the node enters the scene tree for the first time.
@@ -9,4 +9,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("left","right","up","down")
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
